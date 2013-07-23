45cb6f7dc9aaef3a5ae5c8735d3ba27c83e04288
WANG Guoqin <wgq@debian>
2013-07-23 16:55:47 +0800

    Refinerycms Initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Altarf::Application.config.secret_token = '916e8de9a6da70a93b143a678c252a47e15fbcdba031e966e5f3ab77baf318bc3026e4ae7515c602154eec1668a0eb9130505733ce52f0bc71e274c4e59de2c9'
