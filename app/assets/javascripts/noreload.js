c4c21854abf80828a88dc12061c23116b3dbe143
WANG Guoqin <wangguoqin1001@gmail.com>
2013-08-26 02:33:41 +0800

    Another implementation of no-reload using jQuery load()

--- app/assets/javascripts/noreload.js
+++ app/assets/javascripts/noreload.js
@@ -21,24 +21,34 @@ $(document).ready (function() {
 			var url = History.getState().url;
 			$(contentSelector).animate ({opacity: 0.3});
 
-			$.ajax({
-				url: url,
-				timeout: 3000
-			}).done (function (resp) {
+//			$.ajax({
+//				url: url,
+//				timeout: 3000
+//			}).done (function (resp) {
 
-				var $html = $(String(resp));
-				var new_content =  $html.find (contentSelector).html();
+//				var $html = $(String(resp));
+//				var new_content =  $html.find (contentSelector).html();
 
-				$(contentSelector).html (new_content);
-				$(contentSelector).animate ({opacity: 1, visibility: "visible"});
+//				$(contentSelector).html (new_content);
+//				$(contentSelector).animate ({opacity: 1, visibility: "visible"});
 
-				document.title = $html.filter ('title').text();
+//				document.title = $html.filter ('title').text();
 
-				$('body').animate ({scrollTop: $('#menu').offset().top}, 1000);
+//				$('body').animate ({scrollTop: $('#menu').offset().top}, 1000);
 
-			}).fail (function() {
-				document.location.href = url;
-				return false;
+//			}).fail (function() {
+//				document.location.href = url;
+//				return false;
+//			});
+
+			$(contentSelector).load (url + ' ' + contentSelector, function (resp, status, xhr) {
+				if (status == "error") {
+					document.location.href = url;
+				} else {
+					var $html = $(String (resp))
+					$(contentSelector).animate ({opacity: 1, visibility: "visible"});
+					document.title = $html.filter ('title').text();
+				}
 			});
 
 			return false;
