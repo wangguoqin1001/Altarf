566923e2092127e875a5e058fef6b3d55a677949
gld <gld920412@aliyun.com>
2013-08-29 17:02:32 +0800

    change some details and add some new pages by lida

--- config/initializers/refinery/pages.rb
+++ config/initializers/refinery/pages.rb
@@ -41,7 +41,7 @@ Refinery::Pages.configure do |config|
 
    config.layout_template_whitelist = ["application"]
 
-   config.view_template_whitelist = ["home","QZZ-login","QZZ-product-order","QZZ-signup","news-qzz","mdorder","plzregister","member-right","story-about-qzz","introduce-qzz","member-activities","news-qzz-all-words","order-success","future-qzz","product1-qzz","product2-qzz","QZZ-lignzhi-buy","QZZ-lingzhi","QZZ-lingzhi-introduce","QZZ-lingzhi-making","QZZ-lingzhireputation","QZZ-Q10","QZZ-Q10-AQUANOVA","QZZ-Q10-buy","QZZ-Q10-introduce"]
+   config.view_template_whitelist = ["home","QZZ-login","QZZ-product-order","QZZ-signup","news-qzz","mdorder","plzregister","member-right","story-about-qzz","introduce-qzz","member-activities","news-qzz-all-words","order-success","future-qzz","product1-qzz","product2-qzz"]
 
    config.use_layout_templates = true
 
