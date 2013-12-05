1884070103b1a7a0be0e4a42f295e237066a9d7b
WANG Guoqin <wangguoqin1001@gmail.com>
2013-12-06 00:55:49 +0800

    register and login: controller implementation

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,8 +1,13 @@
+#encoding: utf-8
+require 'digest/sha2'
+require 'openssl/cipher'
+require 'base64'
+
 class MembershipsController < ApplicationController
 
 	respond_to :json, :xml, :html
 
-	before_filter :checkcaptcha
+	before_filter :checkcaptcha, :only => [:create, :login]
 
 
 	# GET /memberships
@@ -20,7 +25,7 @@ class MembershipsController < ApplicationController
 
 	# GET /memberships/1
 	def show
-		@membership = Membership.find session[:nickname]
+		@membership = Membership.find :first, :conditions => { :nickname => session[:nickname] }
 		respond_with @membership
 	end
 
@@ -34,13 +39,32 @@ class MembershipsController < ApplicationController
 
 	# GET /memberships/1/edit
 	def edit
-		@membership = Membership.find session[:nickname]
+		@membership = Membership.find :first, :conditions => { :nickname => session[:nickname] }
 		respond_with @membership
 	end
 
 
 	# POST /memberships
 	def create
+		@membership = Membersip.find :first, :conditions => { :nickname => params[:membership][:nickname] }
+		if @membership
+			return { :status => 0, :description => "User exist" }
+		end
+
+		cipher = OpenSSL::Cipher::Cipher.new 'DES3'
+		cipher.encrypt
+		cipher.key = Altarf::Application.config.membership_secret_token
+
+		iv = Digest::SHA256.new
+		iv.update params[:membership][:nickname]
+		cipher.iv = iv.hexdigest
+
+		result = cipher.update params[:membership][:password].slice(0, 23)
+		result << cipher.final
+		pswd = Base64.strict_encode64 result
+
+		params[:membership][:password] = pswd.to_s
+
 		@membership = Membership.new params[:membership]
 		@membership.save
 		respond_with @membership
@@ -49,7 +73,21 @@ class MembershipsController < ApplicationController
 
 	# PUT /memberships/1
 	def update
-		@membership = Membership.find session[:nickname]
+		cipher = OpenSSL::Cipher::Cipher.new 'DES3'
+		cipher.encrypt
+		cipher.key = Altarf::Application.config.membership_secret_token
+
+		iv = Digest::SHA256.new
+		iv.update params[:membership][:nickname]
+		cipher.iv = iv.hexdigest
+
+		result = cipher.update params[:membership][:password].slice(0, 23)
+		result << cipher.final
+		pswd = Base64.strict_encode64 result
+
+		params[:membership][:password] = pswd.to_s
+
+		@membership = Membership.find :first, :conditions => { :nickname => session[:nickname] }
 		@membership.update_attributes params[:membership]
 		respond_with @membership
 	end
@@ -57,9 +95,49 @@ class MembershipsController < ApplicationController
 
 	# DELETE /memberships/1
 	def destroy
-		@membership = Membership.find params[:nickname]
+		@membership = Membership.find :first, :conditions => { :nickname => session[:nickname] }
 		@membership.destroy
 		respond_with @membership
+	end
+
+
+	# POST /memberships/login
+	def login
+		@membership = Membersip.find :first, :conditions => { :nickname => params[:nickname] }
+		if not @membership
+			return { :status => 0, :description => "No such user" }
+		end
+
+		begin
+			pswd = Base64.strict_decode64 @membership[:password]
+
+			cipher = OpenSSL::Cipher::Cipher.new 'DES3'
+			cipher.decrypt
+			cipher.key = Altarf::Application.config.membership_secret_token
+
+			iv = Digest::SHA256.new
+			iv.update params[:nickname]
+			cipher.iv = iv.hexdigest
+
+			clearpswd = cipher.update pswd
+			clearpswd << cipher.final
+
+			hash = Digest::SHA256.new
+			hash.update clearpswd
+			hash.update captcha
+
+		rescue
+			Rails.logger.error $!.backtrace
+			hash = Digest::SHA256.new
+		end
+
+		if hash.hexdigest == params[:password]
+			session[:nickname] = params[:nickname]
+			return { :status => 1 }
+		else
+			session[:nickname] = nil
+			return { :status => 0, :description => "Wrong password" }
+		end
 	end
 
 
