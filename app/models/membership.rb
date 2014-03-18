629c454451466127ba61c6bd936364dead0616c2
WANG Guoqin <wangguoqin1001@gmail.com>
2014-03-18 22:57:45 +0800

    Added :created_at and updated_at into attr_accessible

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,3 +1,3 @@
 class Membership < ActiveRecord::Base
-  attr_accessible :addr, :city, :district, :email, :gender, :mobile, :nickname, :phone, :postal, :province, :username
+  attr_accessible :addr, :city, :district, :email, :gender, :mobile, :nickname, :phone, :postal, :province, :username, :created_at, :updated_at
 end
