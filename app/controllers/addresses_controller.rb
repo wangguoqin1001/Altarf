72dac817544931ad8e69b704478451884c746c40
WANG Guoqin <wangguoqin1001@gmail.com>
2014-04-01 12:25:38 +0800

    addresses controller: add parent

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,48 +2,61 @@ class AddressesController < ApplicationController
 
 	respond_to :json, :xml, :html
 
+	before_filter :load_membership
+
 	# GET /addresses
 	def index
-		@addresses = Address.all
+		@addresses = @membership.addresses.all
 		respond_with @addresses
 	end
 
 	# GET /addresses/1
 	def show
-		@address = Address.find params[:id]
+		@address = @membership.addresses.find params[:id]
 		respond_with @address
 	end
 
 	# GET /addresses/new
 	def new
-		@address = Address.new
+		@address = @membership.addresses.new
 		respond_with @address
 	end
 
 	# GET /addresses/1/edit
 	def edit
-		@address = Address.find params[:id]
+		@address = @membership.addresses.find params[:id]
 		respond_with @address
 	end
 
 	# POST /addresses
 	def create
-		@address = Address.new params[:address]
+		@address = @membership.addresses.new params[:address]
 		@address.save
 		respond_with @address
 	end
 
 	# PUT /addresses/1
 	def update
-		@address = Address.find params[:id]
+		@address = @membership.addresses.find params[:id]
 		@address.update_attributes params[:address]
 		respond_with @address
 	end
 
 	# DELETE /addresses/1
 	def destroy
-		@address = Address.find params[:id]
+		@address = @membership.addresses.find params[:id]
 		@address.destroy
 		respond_with @address
+	end
+
+
+	private
+
+	def load_membership
+		if refinery_user?
+			@membership = Membership.find params[:membership_id]
+		else
+			@membership = Membership.find :first, :conditions => { :nickname => params[:membership][:nickname] }
+		end
 	end
 end
