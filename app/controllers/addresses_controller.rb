bbaa7d4aef6600e8e57b69fffd0c13cd600e4d03
WANG Guoqin <wangguoqin1001@gmail.com>
2014-04-01 12:07:48 +0800

    coding style for addresses controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,83 +1,49 @@
 class AddressesController < ApplicationController
-  # GET /addresses
-  # GET /addresses.json
-  def index
-    @addresses = Address.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @addresses }
-    end
-  end
+	respond_to :json, :xml, :html
 
-  # GET /addresses/1
-  # GET /addresses/1.json
-  def show
-    @address = Address.find(params[:id])
+	# GET /addresses
+	def index
+		@addresses = Address.all
+		respond_with @addresses
+	end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @address }
-    end
-  end
+	# GET /addresses/1
+	def show
+		@address = Address.find params[:id]
+		respond_with @address
+	end
 
-  # GET /addresses/new
-  # GET /addresses/new.json
-  def new
-    @address = Address.new
+	# GET /addresses/new
+	def new
+		@address = Address.new
+		respond_with @address
+	end
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @address }
-    end
-  end
+	# GET /addresses/1/edit
+	def edit
+		@address = Address.find params[:id]
+		respond_with @address
+	end
 
-  # GET /addresses/1/edit
-  def edit
-    @address = Address.find(params[:id])
-  end
+	# POST /addresses
+	def create
+		@address = Address.new params[:address]
+		@address.save
+		respond_with @address
+	end
 
-  # POST /addresses
-  # POST /addresses.json
-  def create
-    @address = Address.new(params[:address])
+	# PUT /addresses/1
+	def update
+		@address = Address.find params[:id]
+		@address.update_attributes params[:address]
+		respond_with @address
+	end
 
-    respond_to do |format|
-      if @address.save
-        format.html { redirect_to @address, notice: 'Address was successfully created.' }
-        format.json { render json: @address, status: :created, location: @address }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @address.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /addresses/1
-  # PUT /addresses/1.json
-  def update
-    @address = Address.find(params[:id])
-
-    respond_to do |format|
-      if @address.update_attributes(params[:address])
-        format.html { redirect_to @address, notice: 'Address was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @address.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /addresses/1
-  # DELETE /addresses/1.json
-  def destroy
-    @address = Address.find(params[:id])
-    @address.destroy
-
-    respond_to do |format|
-      format.html { redirect_to addresses_url }
-      format.json { head :no_content }
-    end
-  end
+	# DELETE /addresses/1
+	def destroy
+		@address = Address.find params[:id]
+		@address.destroy
+		respond_with @address
+	end
 end
