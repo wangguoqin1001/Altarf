c165729726de5ba7f9e4cff8b45880284dd76eba
WANG Guoqin <wangguoqin1001@gmail.com>
2014-04-11 00:16:31 +0800

    HTTPS protocol for products interface

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,22 +1,22 @@
 class Product
 	include HTTParty
-	base_uri 'libra.qzzstore.com'
-
-	Token = { :token => Altarf::Application.config.libra_api_token }
+	base_uri 'https://libra.qzzstore.com'
+	default_params :token => Altarf::Application.config.libra_api_token
 
 
 	def self.all
-		get("/api/variants", { :query => Token })["variants"]
+		get("/api/variants", { :verify => false })["variants"]
 	end
 
 
 	def self.find(sku)
 		get("/api/variants", {
+			:verify => false,
 			:query => {
 				:q => {
 					:sku_eq => sku
 				}
-			}.merge(Token)
+			}
 		})["variants"][0]
 	end
 end
